71cd24559f3419017b39786900ce0695b53a44ae
samuelli <samuelli.pk@gmail.com>
2024-01-16 16:41:16 -0800

    Initial Commit

---
+++ ParkSF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkSF
+//
+//  Created by Samuel Li on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkSF/ParkSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkSFApp.swift
+//  ParkSF
+//
+//  Created by Samuel Li on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
